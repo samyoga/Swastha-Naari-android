67001ba233bec7f7b1bdc7f5e3ef46e5416780bb
samyoga <bhattaraisamyoga@gmail.com>
2019-06-10 22:52:26 +0545

    Project setup

---
+++ app/src/main/java/com/example/swastha_naari/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.swastha_naari;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
